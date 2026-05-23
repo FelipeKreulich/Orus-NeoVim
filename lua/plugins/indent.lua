f2de4450fd6beb4652218f47c7ef748be44b70e1
FelipeKreulich <felipekreulich.pt@gmail.com>
2026-05-23 18:22:19 +0100

    fix: re-define IBL highlight groups before colorscheme autocmd fires
    indent-blankline clears custom highlights on ColorScheme event.
    Registering the define_highlights autocmd before ibl.setup() ensures
    IblIndentRed/Yellow/Blue/Green/Cyan exist when IBL tries to use them,
    fixing the highlights.lua:65 error on all theme switches.

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -20,14 +20,21 @@ return {
 			},
 		},
 		config = function(_, opts)
-			-- Define as cores manualmente
-			vim.cmd([[
-        hi IblIndentRed guifg=#ff5555
-        hi IblIndentYellow guifg=#f1fa8c
-        hi IblIndentBlue guifg=#8be9fd
-        hi IblIndentGreen guifg=#50fa7b
-        hi IblIndentCyan guifg=#8be9fd
-      ]])
+			local function define_highlights()
+				vim.api.nvim_set_hl(0, "IblIndentRed",    { fg = "#ff5555" })
+				vim.api.nvim_set_hl(0, "IblIndentYellow", { fg = "#f1fa8c" })
+				vim.api.nvim_set_hl(0, "IblIndentBlue",   { fg = "#8be9fd" })
+				vim.api.nvim_set_hl(0, "IblIndentGreen",  { fg = "#50fa7b" })
+				vim.api.nvim_set_hl(0, "IblIndentCyan",   { fg = "#8be9fd" })
+			end
+
+			-- Registar ANTES do ibl.setup para correr antes do autocmd interno do IBL
+			-- Garante que os highlight groups existem quando o IBL os tenta usar
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				callback = define_highlights,
+			})
+
+			define_highlights()
 			require("ibl").setup(opts)
 		end,
 	},
